b84c42c34bcab53eec3bb76fb2472e9742f0d96e
Adel Moumen <adelmoumen.pro@gmail.com>
2023-04-03 16:13:01 +0200

    pom updated with path to test directory

--- src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
@@ -1,4 +1,15 @@
 package fr.univavignon.pokedex.api;
+// import mock
+import org.mockito.Mock;
+import org.mockito.Mock.*;
+import org.junit.*;
+
+import fr.univavignon.pokedex.api.IPokemonTrainerFactory;
+import fr.univavignon.pokedex.api.IPokedex;
 
 public class IPokedexFactoryTest {
+
+
+
+
 }
