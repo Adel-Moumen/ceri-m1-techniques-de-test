352a0c4b8b0952e8263d7952bc69e9e053875f7f
Adel Moumen <88119391+Adel-Moumen@users.noreply.github.com>
2023-04-04 11:08:19 +0200

    Update IPokedexTest.java

--- src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
@@ -1,26 +1,34 @@
 package fr.univavignon.pokedex.api;
 
-import org.mockito.Mock;
-import org.mockito.Mock.*;
-import org.junit.*;
-import org.mockito.MockitoAnnotations;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.Collections;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 
 public class IPokedexTest {
 
 
-    @Mock
     private IPokedex pokedex;
-
-
     @Before
-    public void setUp() {
-        // init mock
-        MockitoAnnotations.initMocks(pokedex);
+    public void initTestEnvironment(){
+        IPokemonFactory pokemonFactory = mock(IPokemonFactory.class);
+        IPokedexFactory pokedexFactory = mock(IPokedexFactory.class);
+        IPokemonMetadataProvider metadataProvider = mock(IPokemonMetadataProvider.class);
+        when(pokedexFactory.createPokedex(metadataProvider, pokemonFactory)).thenReturn(mock(IPokedex.class));
+        pokedex = pokedexFactory.createPokedex(metadataProvider, pokemonFactory);
+        when(pokedex.size()).thenReturn(0);
     }
 
-    @After
-    public void tearDown() {
-
-
+    @Test
+    public void testSize() {
+        assert (pokedex.size() == 0);
+        System.out.println("Pokedex Size Test Passed!");
     }
+
 }
