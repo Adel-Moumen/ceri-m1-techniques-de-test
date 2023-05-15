e7d29aac6ac61ca3caa35e95a50c2f2ab9aefd05
Adel Moumen <adelmoumen.pro@gmail.com>
2023-05-15 18:43:21 +0200

    new tests

--- src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -2,27 +2,42 @@ package fr.univavignon.pokedex.api;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.Assert;
 
-import java.util.Objects;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class IPokemonMetadataProviderTest {
 
-    private PokemonMetadata dracofeu;
+    private PokemonMetadataProvider pokemonMetadataProvider1;
+    private PokemonMetadata pokemon1Metadata;
+
+    private IPokemonFactory pokemonFactory;
+    private Pokedex pokedex;
+    private IPokemonMetadataProvider pokemonMetadataProvider2;
+    private PokemonMetadata pokemon2Metadata;
 
     @Before
     public void initTestEnvironment() throws Exception {
-        dracofeu = new PokemonMetadata(0, "Dracofeu", 10000, 10000, 10000);
-        IPokemonMetadataProvider pokemonMetadataProvider = mock(IPokemonMetadataProvider.class);
-        when(pokemonMetadataProvider.getPokemonMetadata(0)).thenReturn(dracofeu);
+        pokemonMetadataProvider1 = new PokemonMetadataProvider();
+        pokemon1Metadata = new PokemonMetadata(0, "pokemon1", 4, 4, 4);
+        pokemon2Metadata = new PokemonMetadata(1, "pokemon2", 384, 29, 500);
+        pokemonFactory = new PokemonFactory();
+        pokedex = new Pokedex(pokemonMetadataProvider1, pokemonFactory);
+        pokemonMetadataProvider2 = pokedex.getPokemonMetadataProvider();
     }
 
     @Test
-    public void testGetPokemonMetadata() {
-        assert (dracofeu != null);
-        System.out.println("Pokemon Metadata Provider Test Passed!");
+    public void assertPokemonMetadaProviderAreTheSame() throws PokedexException {
+        Assert.assertEquals(pokemonMetadataProvider1,pokemonMetadataProvider2);
     }
+
+    @Test
+    public void shouldReturnPokemonMetadata() throws PokedexException {
+        Assert.assertEquals(pokemon1Metadata.getIndex(),pokemonMetadataProvider1.getPokemonMetadata(0).getIndex());
+        Assert.assertEquals(pokemon1Metadata.getName(),pokemonMetadataProvider1.getPokemonMetadata(0).getName());
+        Assert.assertEquals(pokemon1Metadata.getAttack(),pokemonMetadataProvider1.getPokemonMetadata(0).getAttack());
+        Assert.assertEquals(pokemon1Metadata.getDefense(),pokemonMetadataProvider1.getPokemonMetadata(0).getDefense());
+        Assert.assertEquals(pokemon1Metadata.getStamina(),pokemonMetadataProvider1.getPokemonMetadata(0).getStamina());
+    }
+
 }
